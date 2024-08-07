3775ab34e41d5d2c9858c78254fc90c15d2b1da5
brunom764 <brunom764@gmail.com>
2024-08-07 16:27:07 -0300

    --x

--- src/example-2/todoList.spec.ts
+++ src/example-2/todoList.spec.ts
@@ -7,33 +7,33 @@ describe('TodoList', () => {
     todoList = new TodoList();
   });
 
-  test('should add a new todo', () => {
+  it('should add a new todo', () => {
     todoList.addTodo('Learn Jest');
     expect(todoList.getTodos()).toEqual([{ task: 'Learn Jest', completed: false }]);
   });
 
-  test('should not add an invalid todo', () => {
+  it('should not add an invalid todo', () => {
     expect(() => todoList.addTodo('')).toThrow('Invalid task');
   });
 
-  test('should remove a todo', () => {
+  it('should remove a todo', () => {
     todoList.addTodo('Learn Jest');
     todoList.removeTodo('Learn Jest');
     expect(todoList.getTodos()).toEqual([]);
   });
 
-  test('should mark a todo as completed', () => {
+  it('should mark a todo as completed', () => {
     todoList.addTodo('Learn Jest');
     todoList.markCompleted('Learn Jest');
     expect(todoList.getTodos()).toEqual([{ task: 'Learn Jest', completed: true }]);
   });
 
-  test('should not mark a non-existing todo as completed', () => {
+  it('should not mark a non-existing todo as completed', () => {
     todoList.markCompleted('Non-existing task');
     expect(todoList.getTodos()).toEqual([]);
   });
 
-  test('should get all todos', () => {
+  it('should get all todos', () => {
     todoList.addTodo('Learn Jest');
     todoList.addTodo('Write Tests');
     expect(todoList.getTodos()).toEqual([
