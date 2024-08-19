a9c33792bda24088cb646859167950077a530681
tiagochacon <tiagosennachacon@gmail.com>
2024-08-19 00:16:31 -0300

    Adicionar casos de teste para a classe Zoo

--- src/challenge/zoo.spec.ts
+++ src/challenge/zoo.spec.ts
@@ -1,4 +1,3 @@
-
 import { Zoo, Animal } from './zoo';
 
 describe('Zoo', () => {
@@ -14,5 +13,67 @@ describe('Zoo', () => {
     expect(zoo.getAllAnimals()).toContainEqual(animal);
   });
 
-  // Add more tests here
+  test('should remove an existing animal', () => {
+    const animal1 = new Animal('Leo', 'Lion', 5);
+    const animal2 = new Animal('Milo', 'Tiger', 3);
+    zoo.addAnimal(animal1);
+    zoo.addAnimal(animal2);
+    zoo.removeAnimal('Leo');
+    expect(zoo.getAllAnimals()).not.toContainEqual(animal1);
+    expect(zoo.getAllAnimals()).toContainEqual(animal2);
+  });
+
+  test('should not remove an animal that does not exist', () => {
+    const animal = new Animal('Leo', 'Lion', 5);
+    zoo.addAnimal(animal);
+    zoo.removeAnimal('Nonexistent');
+    expect(zoo.getAllAnimals()).toContainEqual(animal);
+  });
+
+  test('should get an animal by name', () => {
+    const animal = new Animal('Leo', 'Lion', 5);
+    zoo.addAnimal(animal);
+    expect(zoo.getAnimal('Leo')).toEqual(animal);
+  });
+
+  test('should return undefined for a non-existent animal', () => {
+    expect(zoo.getAnimal('Nonexistent')).toBeUndefined();
+  });
+
+  test('should get all animals', () => {
+    const animal1 = new Animal('Leo', 'Lion', 5);
+    const animal2 = new Animal('Milo', 'Tiger', 3);
+    zoo.addAnimal(animal1);
+    zoo.addAnimal(animal2);
+    expect(zoo.getAllAnimals()).toEqual([animal1, animal2]);
+  });
+
+  test('should get animals by species', () => {
+    const lion1 = new Animal('Leo', 'Lion', 5);
+    const lion2 = new Animal('Simba', 'Lion', 4);
+    const tiger = new Animal('Milo', 'Tiger', 3);
+    zoo.addAnimal(lion1);
+    zoo.addAnimal(lion2);
+    zoo.addAnimal(tiger);
+    expect(zoo.getAnimalsBySpecies('Lion')).toEqual([lion1, lion2]);
+    expect(zoo.getAnimalsBySpecies('Tiger')).toEqual([tiger]);
+  });
+
+  test('should return an empty array when no animals of a species exist', () => {
+    const animal = new Animal('Leo', 'Lion', 5);
+    zoo.addAnimal(animal);
+    expect(zoo.getAnimalsBySpecies('Tiger')).toEqual([]);
+  });
+
+  test('should get the average age of animals', () => {
+    const animal1 = new Animal('Leo', 'Lion', 5);
+    const animal2 = new Animal('Milo', 'Tiger', 3);
+    zoo.addAnimal(animal1);
+    zoo.addAnimal(animal2);
+    expect(zoo.getAverageAge()).toBe(4);
+  });
+
+  test('should return 0 as average age when there are no animals', () => {
+    expect(zoo.getAverageAge()).toBe(0);
+  });
 });
